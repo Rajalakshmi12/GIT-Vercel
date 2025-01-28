4977301462884d71f5ecfd82680dc27b6df2e1a8
Rajalakshmi Rathinasabhapathy <poorthi.laxmi@gmail.com>
2025-01-28 21:16:59 +0000

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,2 @@
-# api/index.py
-import json
-from http.server import BaseHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps({"message": "Hello!"}).encode('utf-8'))
-        return
+print('Hello Raji!')
+print(os.environ.get('WHICH_ENV'))
